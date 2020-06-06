d20ed00989ebaed33be0bd21973685cf618537b7
muskan maheshwari <muskaan.maheshwari02@gmail.com>
2020-06-06 23:49:40 +0530

    m

--- static/js/todos.js
+++ static/js/todos.js
@@ -1,17 +1,23 @@
 // alert("CONNECTED!!");
 
-//strike todos by clicking
-// $("ul").on("click", "li", function(){
-// 	$(this).toggleClass("completed");	
-// });
-
-
-var li = document.querySelector("li");
-li.addEventListener("click", function(){
-	this.classList.toggle("completed");
+// strike todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");	
 });
 
 
+// var li = document.querySelectorAll("li");
+// li.addEventListener("click", function(){
+// 	this.classList.toggle("completed");
+// });
+
+//click on X to delete
+$("ul").on("click", "button", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation(); //stop event bubbling
+})
 
 
 
