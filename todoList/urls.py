40534f59ad4ddea245652dc74c3fa4e12c741d3e
muskan maheshwari <muskaan.maheshwari02@gmail.com>
2020-06-06 22:20:20 +0530

    m

--- todoList/urls.py
+++ todoList/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('index/', views.index, name='index'),
 	path('add', views.add, name='add'),
 	path('delete/<int:todo_id>/', views.delete, name='delete')
 ]
