3a5650e2ddeabd4645c570238a6f9f0c34a3ba25
muskan maheshwari <muskaan.maheshwari02@gmail.com>
2020-06-06 16:44:29 +0530

    req up

--- todos/settings.py
+++ todos/settings.py
@@ -15,6 +15,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todoList',
+    'pwa',
 ]
 
 MIDDLEWARE = [
--- todos/urls.py
+++ todos/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todoList.urls'))
+    path('', include('todoList.urls')),
+    path('',include('pwa.urls')),
 ]
