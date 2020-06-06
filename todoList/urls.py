24d6ad516ce6cfc49a52c9fa83ae77b0e11b9023
muskan maheshwari <muskaan.maheshwari02@gmail.com>
2020-06-06 23:02:16 +0530

    m

--- todoList/urls.py
+++ todoList/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('index1', views.index1, name='index1'),
 	path('add', views.add, name='add'),
 	path('delete/<int:todo_id>/', views.delete, name='delete')
 ]
--- todoList/views.py
+++ todoList/views.py
@@ -7,23 +7,27 @@ def index(request):
 	data=Todo.objects.all()
 	return render(request, 'index.html', {'data' : data})
 
+def index1(request):
+	data=Todo.objects.all()
+	return render(request, 'index.html', {'data' : data})
+
 def add(request):
 	if request.method=='POST':
 		todo=request.POST['todo']
 		if todo!="":
 			todoItem=Todo(title=todo)
 			todoItem.save()
-			return redirect('index')
+			return redirect('index1')
 		else:
-			return redirect('index')
+			return redirect('index1')
 	else:
-		return redirect('index')
+		return redirect('index1')
 
 def delete(request, todo_id):
 	if request.method=='POST':
 		item=Todo.objects.get(id=todo_id)
 		item.delete()
-		return redirect('index')
+		return redirect('index1')
 	else:
-		return redirect('index')
+		return redirect('index1')
 
