cafa1ff1cf95170e5e171fae1cfecfbf07be20b5
muskan maheshwari <muskaan.maheshwari02@gmail.com>
2020-06-06 22:30:33 +0530

    m

--- todos/urls.py
+++ todos/urls.py
@@ -17,12 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index,name='index'),
     path('', include('todoList.urls')),
     path('',include('pwa.urls')),
 ]
