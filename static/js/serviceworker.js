199000ed155a9cef3d6a872f5b94df274f4adef6
muskan maheshwari <muskaan.maheshwari02@gmail.com>
2020-06-08 16:25:08 +0530

    add

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        'index',
+        '',
       ]);
     })
   );
@@ -14,7 +14,7 @@ self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
       if ((requestUrl.pathname === '/')) {
-        event.respondWith(caches.match('index'));
+        event.respondWith(caches.match(''));
         return;
       }
     }
