b77eadbdb5b561cf17a1282cbde4ef14d40bb6bf
muskan maheshwari <muskaan.maheshwari02@gmail.com>
2020-06-09 22:14:16 +0530

    added

--- todoList/urls.py
+++ todoList/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	path('index', views.index, name='index'),
 	# path('index1', views.index1, name='index1'),
 	path('add', views.add, name='add'),
+	path('addtheme', views.addtheme, name='addtheme'),
 	path('delete/<int:todo_id>/', views.delete, name='delete'),
 	path('completed/<int:todo_id>/', views.completed, name='completed'),
 	path('notcompleted/<int:todo_id>/', views.notcompleted, name='notcompleted'),
